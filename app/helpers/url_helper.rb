46dadafe5e2d53c523f957f2d94f54d3861b8aca
Andrew Carpenter <acarpen@wested.org>
2011-09-09 16:51:49 -0700

    link to FR2

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -18,4 +18,8 @@ module UrlHelper
   def document_pdf_url(obj)
     "http://www.gpo.gov/fdsys/pkg/FR-#{obj.publication_date.to_s(:db)}/pdf/#{obj.document_number}.pdf"
   end
+
+  def fr2_url(obj)
+    "http://federalregister.gov/a/#{obj.document_number}"
+  end
 end
