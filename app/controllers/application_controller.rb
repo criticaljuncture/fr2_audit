a359936277d17f29aa850a05f4b58cbf1035b409
Andrew Carpenter <acarpen@wested.org>
2011-09-12 13:30:57 -0700

    basic HTTP digest auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_digest do |username|
+      self.class.valid_users[username]
+    end
+  end
+
+  def self.valid_users
+    @valid_users ||= YAML.load_file(Rails.root.join('config', 'secrets.yml'))['users']
+  end
 end
